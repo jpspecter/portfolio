256a0d1da0faee7715e85cfc4589dfd11f680bfe
Janam Parikh <janamparikh08@gmail.com>
2020-07-19 21:14:36 -0500

    Updates

---
+++ precache-manifest.707f8222dc4d7f35525afaf607c3fba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad704c1fbd2d96955f13320806d6c2b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c134aa5337edd028a163",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "204a3a0cae7b62f9b255",
+    "url": "/portfolio/static/js/2.5f7d4ff1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.5f7d4ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c134aa5337edd028a163",
+    "url": "/portfolio/static/js/main.2fcc58cf.chunk.js"
+  },
+  {
+    "revision": "db39b0e1f2ee7d3a5637",
+    "url": "/portfolio/static/js/runtime-main.ad3b6095.js"
+  }
+]);
